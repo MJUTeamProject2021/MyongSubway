288482a5c5b15b418bf3ef89d18dd91711f8fb40
MadeByZiNo <38098073+MadeByZiNo@users.noreply.github.com>
2021-10-30 18:09:23 +0900

    Main 거의 완성

--- app/src/main/java/com/example/myongsubway/MainActivity.java
+++ app/src/main/java/com/example/myongsubway/MainActivity.java
@@ -37,24 +37,35 @@ public class    MainActivity extends AppCompatActivity implements View.OnClickLi
 
     //public StationInformationFragment fragment;
     public StationReportFragment fragment;
-    public Button findButton;
-    public Button changeButton;
-    public Button gotoShort;
-    public Button gotoBookmark;
-    public Button gotoSearch;
-    public Button gotoSetting;
+    private Button findButton;
+    private Button changeButton;
+    private Button gotoShort;
+    private Button gotoBookmark;
+    private Button gotoSearch;
+    private Button gotoSetting;
     public TextView departText;
     public TextView destiText;
     public Intent intent;
+    private CustomAppGraph graph;
     public ArrayList<Button>stationButtonList = new ArrayList();
+    public ArrayList<CustomAppGraph.Vertex> mainVertices;
 
-    @Override
+
+
+    @Override        //초기화 메소드
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        for(int i=0;i<111;i++) {
-            //ID 동적으로 받기
+        //액션바 가리기
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.hide();
+
+        graph = (CustomAppGraph) getApplicationContext();        //공유되는 데이터 담는 객체
+        mainVertices = graph.getVertices();
+
+        //지하철버튼들을 동적으로 연결, 클릭리스너 설정
+        for(int i=0;i<graph.getStationCount();i++) {
             String tempId =  "main_Button_Station" + (i+1) ;
             int resID = getResources().getIdentifier(tempId, "id", getPackageName());
             Button button = findViewById(resID);
@@ -62,9 +73,6 @@ public class    MainActivity extends AppCompatActivity implements View.OnClickLi
             stationButtonList.get(i).setOnClickListener(this);
         }
 
-        //액션바 가리기
-        ActionBar actionBar = getSupportActionBar();
-        actionBar.hide();
         //버튼 ID등록
         findButton = findViewById(R.id.Main_Button_Find);
         changeButton = findViewById(R.id.Main_Button_Change);
@@ -84,68 +92,31 @@ public class    MainActivity extends AppCompatActivity implements View.OnClickLi
         gotoShort.setOnClickListener(this);
     }
 
-    //역 클릭
-  /*  void onPhotoTap (ImageView view ,float  x ,float  y )
-    {
-        ArrayList<String> adjacent_ = new ArrayList();
-        ArrayList<String> facilities_ = new ArrayList();
-        if(x>0.03275892&& y>0.020183377&&x<0.065530114&& y<0.0830131 ) {
-            String a,b;  a = "204";  b = "206";
-            adjacent_.add(a); adjacent_.add(b);
-            String c;  c = "세븐일레븐 205역점 ";
-            facilities_.add(c);
-           // fragment = new StationInformationFragment("205역",adjacent_,facilities_,2,false);
-            fragment = new StationReportFragment();
-        }
-        else if(x>0.027996428&& y>0.10434187&&x<0.06481509&& y<0.15522596 ){
-            String a,b;  a = "203";  b = "205";
-            adjacent_.add(a); adjacent_.add(b);
-            fragment = new StationInformationFragment("204역",adjacent_,facilities_,2,false);
-        }
-        else if(x>0.035435267&& y>0.18798444&&x<0.06665038&& y<0.23491357 ){
-            String a,b;  a = "202";  b = "204";
-            adjacent_.add(a); adjacent_.add(b);
-            String c,d;  c = "교보문고 203역점 "; d = "맛나분식 203역점";
-            facilities_.add(c); facilities_.add(d);
-            fragment = new StationInformationFragment("203역",adjacent_,facilities_,2,false);
-        }
-        else if(x>0.036504436&& y>0.26615265&&x<0.061516732&& y<0.30894282 ) {
-            String a,b,f;  a = "201";  b = "203"; f = "303";
-            adjacent_.add(a); adjacent_.add(b); adjacent_.add(f);
-            String c,d,e;  c = "GS25 202역점 ";  d = "202 빵가게"; e = "휴대폰성지 202역점";
-            facilities_.add(c); facilities_.add(d); facilities_.add(e);
-            fragment = new StationInformationFragment("202역",adjacent_,facilities_,2,true);
-        }
-        else {
-            return;
-        }
-        FragmentTransaction transaction  = getSupportFragmentManager().beginTransaction();
-        transaction.replace(R.id.Main_ConstraintLayout_Main, fragment);
-        transaction.commit();
-    }*/
 
-    //프래그먼트 파괴
+    //Report 프래그먼트 종료
     void destroyFragment(){
         FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
         fragmentTransaction.remove(fragment);
         fragmentTransaction.commit();
     }
 
-    //터치
+    //터치할시에
     @Override
     public void onClick(View view) {
 
-        for(int i=0;i<111;i++) {
+        //역 버튼을 클릭했는지 확인하는 반복문
+        for(int i=0;i<graph.getStationCount();i++) {
             if(view.getId() ==stationButtonList.get(i).getId()) {
-                System.out.println(stationButtonList.get(i).getId());
-                fragment = new StationReportFragment();
+                fragment = new StationReportFragment(mainVertices.get(i),mainVertices,graph,mainVertices.get(i).getLine());
                 FragmentTransaction transaction  = getSupportFragmentManager().beginTransaction();
                 transaction.replace(R.id.Main_ConstraintLayout_Main, fragment);
                 transaction.commit();
             }
             }
+
+        //상단과 하단의 메뉴버튼들 클릭확인하는 스위치문
         switch (view.getId()) {
-            case R.id.Main_Button_Find:
+            case R.id.Main_Button_Find:                                             //출발역과 도착역 넘기기
                 if(destiText.length() ==0 || departText.length() ==0){
                     Toast.makeText(this, "출발역과 도착역을 모두 입력하지 않았습니다.", Toast.LENGTH_SHORT).show();
                 }
@@ -154,11 +125,11 @@ public class    MainActivity extends AppCompatActivity implements View.OnClickLi
                     String desti  = destiText.getText().toString();
                     Intent intent = new Intent(this,ShortestPathActivity.class);
                     intent.putExtra("departureStation",depart);
-                    intent.putExtra("DestinationStation",desti);
+                    intent.putExtra("destinationStation",desti);
                     startActivity(intent);
                 }
                 break;
-            case R.id.Main_Button_Change:
+            case R.id.Main_Button_Change:                                       //출발역과도착역 바꾸기
                     String temp = departText.getText().toString();
                     departText.setText(destiText.getText());
                     destiText.setText(temp);
