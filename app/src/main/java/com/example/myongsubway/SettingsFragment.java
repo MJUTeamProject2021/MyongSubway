c83d3cbc28bb3218a82d435b581aaabd1a72dadb
An Ji Hun <halo4945@gmail.com>
2021-11-04 17:34:15 +0900

    지메일 인텐트 알림소리 추가 알림 확인 진동은 휴대폰 연결 후 확인 가능할 듯 함

--- app/src/main/java/com/example/myongsubway/SettingsFragment.java
+++ app/src/main/java/com/example/myongsubway/SettingsFragment.java
@@ -1,8 +1,18 @@
 package com.example.myongsubway;
 
+
+
+import android.content.Context;
 import android.content.SharedPreferences;
+import android.media.MediaPlayer;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.VibrationEffect;
+import android.os.Vibrator;
+import android.widget.Toast;
+
 import androidx.preference.ListPreference;
+import androidx.preference.Preference;
 import androidx.preference.PreferenceFragmentCompat;
 import androidx.preference.PreferenceManager;
 
@@ -11,7 +21,7 @@ import androidx.preference.PreferenceManager;
 public class SettingsFragment extends PreferenceFragmentCompat {
 
     SharedPreferences prefs;//설정 저장
-    ListPreference ringtonePreference;//벨소리
+    Preference ringtonePreference;//벨소리
 
     @Override
     public void onCreatePreferences(Bundle savedInstanceState, String s) {
@@ -33,9 +43,31 @@ public class SettingsFragment extends PreferenceFragmentCompat {
         public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String s) {//설정 변경 리스너
             if(s.equals("ringtone_list")) {
                 ringtonePreference.setSummary(prefs.getString("ringtone_list", "기본"));//키에서 찾아서 리턴
-                
+                Toast myToast = Toast.makeText(getActivity(),prefs.getString("ringtone_list", "기본"), Toast.LENGTH_SHORT);
+                myToast.show();
             }
-
+            else if(prefs.getBoolean("ring", true)){//.벨소리 확인 코드 삭제 가능
+                if(prefs.getString("ringtone_list","").equals("기본")){
+                    MediaPlayer player = MediaPlayer.create(getActivity(), R.raw.clock);// ** listPreference에서 알림 파일 연결 어떻게?
+                    player.start();
+                }else if(prefs.getString("ringtone_list","").equals("카톡")){
+                    MediaPlayer player = MediaPlayer.create(getActivity(), R.raw.kakao);// ** listPreference에서 알림 파일 연결 어떻게?
+                    player.start();
+                }else if(prefs.getString("ringtone_list","").equals("카톡카톡")){
+                    MediaPlayer player = MediaPlayer.create(getActivity(), R.raw.kakaokakao);// ** listPreference에서 알림 파일 연결 어떻게?
+                    player.start();
+                }
+            }
+            else if(prefs.getBoolean("sneeze",true)){//진동확인 코드 삭제 가능
+                Vibrator vib = (Vibrator)getActivity().getSystemService(Context.VIBRATOR_SERVICE);
+                Toast myToast = Toast.makeText(getActivity(),"진동", Toast.LENGTH_SHORT);
+                myToast.show();
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                    vib.vibrate(VibrationEffect.createOneShot(1000, VibrationEffect.DEFAULT_AMPLITUDE));
+                } else {
+                    vib.vibrate(1000);
+                }
+            }
            //((BaseAdapter)getPreferenceScreen().getRootAdapter()).notifyDataSetChanged();
         }
     };
