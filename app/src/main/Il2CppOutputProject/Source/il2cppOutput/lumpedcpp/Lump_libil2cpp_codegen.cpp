7a3563c428ef7996dc4c3599e60bf0939e1fedb0
carefulseop510 <wndtjq0510@naver.com>
2021-12-01 19:21:03 +0900

    no message

--- app/src/main/Il2CppOutputProject/Source/il2cppOutput/lumpedcpp/Lump_libil2cpp_codegen.cpp
+++ app/src/main/Il2CppOutputProject/Source/il2cppOutput/lumpedcpp/Lump_libil2cpp_codegen.cpp
@@ -1,4 +1,4 @@
 #include "il2cpp-config.h"
-#include "C:\Users\jh990\Documents\MyongSubway\app\src\main\Il2CppOutputProject\IL2CPP\libil2cpp\codegen\il2cpp-codegen-common.cpp"
-#include "C:\Users\jh990\Documents\MyongSubway\app\src\main\Il2CppOutputProject\IL2CPP\libil2cpp\codegen\il2cpp-codegen-il2cpp.cpp"
-#include "C:\Users\jh990\Documents\MyongSubway\app\src\main\Il2CppOutputProject\IL2CPP\libil2cpp\codegen\il2cpp-codegen-tiny.cpp"
+#include "C:\Development\Android\MyongSubway_carefulseop510\app\src\main\Il2CppOutputProject\IL2CPP\libil2cpp\codegen\il2cpp-codegen-common.cpp"
+#include "C:\Development\Android\MyongSubway_carefulseop510\app\src\main\Il2CppOutputProject\IL2CPP\libil2cpp\codegen\il2cpp-codegen-il2cpp.cpp"
+#include "C:\Development\Android\MyongSubway_carefulseop510\app\src\main\Il2CppOutputProject\IL2CPP\libil2cpp\codegen\il2cpp-codegen-tiny.cpp"
